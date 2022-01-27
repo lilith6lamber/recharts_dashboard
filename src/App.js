fca09438e9cdd2e1cc85e7858f8976cce7a6f6ce
Lilith Lamber <winter.rituel@gmail.com>
2022-01-27 22:36:13 +0200

    windowWidth using custom hook

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
-import {Component} from "react";
+import {useWindowWidth} from '@react-hook/window-size'
 import './scss/style.scss';
 import Sidebar from "./components/Sidebar";
 import DashboardApp from "./components/DashboardApp";
 
-class App extends Component {
-    state = {
-        windowWidth: window.innerWidth
-    }
-
-    updateWidth() {
-        this.setState({windowWidth: window.innerWidth})
-    }
-
-    componentDidMount() {
-        this.updateWidth();
-    }
-
-    render () {
-        const {windowWidth} = this.state;
-        return (
-            <>
-                <div className="dashboard">
-                    <Sidebar windowWidth={windowWidth} />
-                    <DashboardApp windowWidth={windowWidth} />
-                </div>
-                <div className="shape shape--1"></div>
-                <div className="shape shape--2"></div>
-                <div className="shape shape--3"></div>
-            </>
-        )
-    }
+function App() {
+    const windowWidth = useWindowWidth();
+    return (
+        <>
+            <div className="dashboard">
+                <Sidebar windowWidth={windowWidth} />
+                <DashboardApp windowWidth={windowWidth} />
+            </div>
+            <div className="shape shape--1"></div>
+            <div className="shape shape--2"></div>
+            <div className="shape shape--3"></div>
+        </>
+    )
 }
 
 export default App;
